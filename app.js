ede870a0f10313a929a76bcc232c9b330c0d0ba4
Trung Dinh Duc (FIS SRVHCM) <trungdd9@fpt.com.vn>
2020-02-14 15:42:30 +0700

    Mesage

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
- 
+const option = {
+    username: 'admin',
+
+    password: '123qwe!@#',
+    port: 1883,
+    host: '172.30.67.117'
+}
+
+
+const client = mqtt.connect('mqtt://172.30.67.117:1883', option)
 client.on('connect', function () {
   client.subscribe('presence', function (err) {
     if (!err) {
