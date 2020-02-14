9619efccc0270d900cb331ecae10b3fbd286ca6e
Trung Dinh Duc (FIS SRVHCM) <trungdd9@fpt.com.vn>
2020-02-14 14:30:54 +0700

    message

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('172.30.235.164:1883')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
  
 client.on('connect', function () {
   client.subscribe('presence', function (err) {
     if (!err) {
       client.publish('presence', 'Hello mqtt')
+    }
+    else 
+    {
+        console.log("Err "+err)
     }
   })
 })
